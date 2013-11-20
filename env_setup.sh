bebb45c50aeb9e87565fa9bb2f2be18ccfa39631
Pranav Vashi <neobuddy89@gmail.com>
2013-11-20 11:48:33 -0800

    build: Use linaro toolchain
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- env_setup.sh
+++ env_setup.sh
@@ -38,7 +38,7 @@ export TMPFILE=`mktemp -t`;
 
 # system compiler
 # export CROSS_COMPILE=$PARENT_DIR/linaro-toolchain-4.8-2013.10/bin/arm-eabi-;
-export CROSS_COMPILE=$PARENT_DIR/arm-eabi-4.7/bin/arm-eabi-;
+export CROSS_COMPILE=$PARENT_DIR/linaro-toolchain-4.7-2013.10/bin/arm-eabi-;
 
 export NUMBEROFCPUS=`grep 'processor' /proc/cpuinfo | wc -l`;
 
