dca407db3fa34435859173a6133efe8327619d64
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-01-02 17:59:29 +0700

    WIP: aries-cma
    TODO:
    - Implement CMA for FIMC. AFAIK FIMC0 is FFC and FIMC2 is TV-Out
    
    - Make sure driver does not write to memory while not allocated
    
    - Iron out allocation failures:
      - Having zram enabled will result in some "page allocation failure".
        this also happens without zram, but with lower frequency. Cause
        unknown, but may be related to the code putting CMA-allocated
        page back into the kernel allocator after deallocation.
      - Having KSM enabled will increase the chance of CMA allocation
        failure (for device). This needs to be resolved.
      - CMA allocation may fail. This may need to be handled gracefully
        in the userspace driver.
      - Stress-test for CMA allocation: "while cat /dev/s3c-jpg; do done"
        This will open and close s3c-jpg which repeated allocate and
        deallocate the CMA area until there is a failure. Using the device
        during this stress-test will increase the chance of a failure and
        leaving the device on for a longtime will also incrase the chance
        of a failure. This needs to be resolved.
    
    - Maybe use the central CMA area except for MFC which specifically
      requires 2 banks

--- arch/arm/mach-s5pv210/mach-aries.c
+++ arch/arm/mach-s5pv210/mach-aries.c
@@ -339,6 +339,7 @@ static struct s5p_media_device aries_media_devs[] = {
 		.bank = 0,
 		.memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_MFC0,
 		.paddr = 0,
+		.cmadev = &s3c_device_mfc_l.dev,
 	},
 	[1] = {
 		.id = S5P_MDEV_MFC,
@@ -346,6 +347,7 @@ static struct s5p_media_device aries_media_devs[] = {
 		.bank = 1,
 		.memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_MFC1,
 		.paddr = 0,
+		.cmadev = &s3c_device_mfc_r.dev,
 	},
 	[2] = {
 		.id = S5P_MDEV_FIMC0,
@@ -353,6 +355,7 @@ static struct s5p_media_device aries_media_devs[] = {
 		.bank = 1,
 		.memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC0,
 		.paddr = 0,
+		// .cmadev = &s3c_device_fimc0.dev,
 	},
 /*	[3] = {
 		.id = S5P_MDEV_FIMC1,
@@ -360,6 +363,7 @@ static struct s5p_media_device aries_media_devs[] = {
 		.bank = 1,
 		.memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC1,
 		.paddr = 0,
+		.cmadev = &s3c_device_fimc1.dev,
 	},*/
 	[4] = {
 		.id = S5P_MDEV_FIMC2,
@@ -367,6 +371,7 @@ static struct s5p_media_device aries_media_devs[] = {
 		.bank = 1,
 		.memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_FIMC2,
 		.paddr = 0,
+		// .cmadev = &s3c_device_fimc2.dev,
 	},
 	[5] = {
 		.id = S5P_MDEV_JPEG,
@@ -374,6 +379,7 @@ static struct s5p_media_device aries_media_devs[] = {
 		.bank = 0,
 		.memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_JPEG,
 		.paddr = 0,
+		.cmadev = &s3c_device_jpeg.dev,
 	},
 	[6] = {
 		.id = S5P_MDEV_FIMD,
@@ -5073,6 +5079,8 @@ static struct platform_device *aries_devices[] __initdata = {
 
 #ifdef CONFIG_VIDEO_MFC50
 	&s3c_device_mfc,
+	&s3c_device_mfc_l,
+	&s3c_device_mfc_r,
 #endif
 #ifdef	CONFIG_S5P_ADC
 	&s3c_device_adc,
